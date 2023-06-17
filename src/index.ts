fba197ca1383229cc04560b59c90c9c9478ce05c
James Chen <78395974+invacctvni@users.noreply.github.com>
2023-06-17 13:27:48 -0400

    create user model, connect to ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import  {json} from 'body-parser'
 import { mongoConnection } from "./initialDB"
 import Todo from './models/todo.model'
 require('dotenv-flow').config() 
-
+import {User} from "./models/user.model"
 
 
 const app = express()
@@ -12,10 +12,29 @@ app.use(json())
 //router: 
 app.use('/', (req: Request, res: Response) => {
 
-    const title = "abc"
-    const description = "desc"
-    const todo = Todo.build({title, description})
-    res.send({rs:true, data: todo})
+    // const title = "abc"
+    // const description = "desc"
+    // const todo = Todo.build({title, description})
+    // res.send({rs:true, data: todo})
+
+
+    run().catch(err => console.log(err));
+
+    async function run() {
+    // 4. Connect to MongoDB
+    // await connect('mongodb://127.0.0.1:27017/test');
+
+    const user = new User({
+        name: 'Bill',
+        email: 'bill@initech.com',
+        avatar: 'https://i.imgur.com/dM7Thhn.png'
+    });
+    await user.save();
+
+    console.log(user.email); // 'bill@initech.com'
+    res.send({rs:true, data: user})    
+}
+
 })
 //connect to MongoDB Atlas
 mongoConnection()
@@ -23,4 +42,5 @@ app.listen(process.env.DATABASE_PORT, () => {
     // console.log(process.env);  
     console.log('Server is listening ');
 })
+
 
